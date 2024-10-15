0d074ec0aa2308299683150f3fff9ad35031f6b5
KevinFlorenciaMoragas <kevinflomor@campus.monlau.com>
2024-10-15 08:15:48 +0200

    15/10

---
+++ Peliculas_react/src/components/CardRelationMovie.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function CardRelationMovie({id, genreId}) {
+    const[relationMovies, setRelationMovies] = useState({})
+    
+
+  return (
+    <>
+    </>
+  )
+}
--- Peliculas_react/src/pages/MovieDetail.jsx
+++ Peliculas_react/src/pages/MovieDetail.jsx
@@ -2,11 +2,13 @@ import { Tab } from 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Tabs from 'react-bootstrap/esm/Tabs';
+import CardRelationMovie from '../components/CardRelationMovie';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function MovieDetail() {
   const [movie, setMovie] = useState(null);
+  const [moviesRelation, setMoviesRelation] = useState({})
   const [errorMessage, setErrorMessage] = useState("");
   const { id } = useParams();
 
@@ -32,14 +34,15 @@ export default function MovieDetail() {
       .catch(() => {
         setErrorMessage('Fallo al cargar la película');
       });
+
   }, [id]);
 
   const imgStyle = {
-    width: '5em',
+    width: '20%',
     height: 'auto'
   };
 
-  if (movie === null) {
+  if (movie == null) {
     return <h1>Cargando...</h1>;
   }
 
@@ -50,11 +53,11 @@ export default function MovieDetail() {
           <h2 className='pt-2'>{movie.movieName}</h2>
           <span className='pt-2'>Año {movie.releaseDate} | Duración {movie.duration} min</span>
           <img className='img-fluid d-lg-block' src={movie.banner} alt={`${movie.movieName} banner`} />
-          <article className='col-12 col-md-5 col-lg-4 p-3 d-flex'>
+          <article className='col-12 col-md-5 col-lg-12 p-3 d-flex'>
             <img className='img-fluid' src={movie.poster} style={imgStyle} alt={`${movie.movieName} poster`} />
             <div className='p-2 flex-column'>
               <div className='d-flex'>
-                {movie.genres.map((genre, index) => (
+                {movie.genres && movie.genres.map((genre, index) => (
                   <span key={index} className='badge bg-primary m-1'>{genre.name}</span>
                 ))}
               </div>
@@ -63,13 +66,40 @@ export default function MovieDetail() {
           </article>
           <section className='col-12 col-md-5 col-lg-4 p-3'>
             <h3>Director</h3>
-            <span>{movie.director.name} {movie.director.lastName}</span>
+            {movie.directors && movie.directors.length > 0 ? (
+              movie.directors.map((directors, index) => (
+                <span key={index}> {directors.name} {directors.lastName} </span>
+              ))
+            ) : (
+              <span>Sin actores disponibles</span>
+            )}
             <h3>Actores</h3>
             <div>
-              {movie.actors.map((actor, index) => (
-                <span key={index}> {actor.name} {actor.lastName} </span>
-              ))}
+              {movie.actors && movie.actors.length > 0 ? (
+                movie.actors.map((actor, index) => (
+                  <span key={index}> {actor.name} {actor.lastName} </span>
+                ))
+              ) : (
+                <span>Sin actores disponibles</span>
+              )}
             </div>
+            <h3>Escritores</h3>
+            <div>
+              {movie.screenwritters && movie.screenwritters.length > 0 ? (
+                movie.screenwritters.map((screenwritters, index) => (
+                  <span key={index}> {screenwritters.name} {screenwritters.lastName} </span>
+                ))
+              ) : (
+                <span>Sin escritores disponibles</span>
+              )}
+            </div>
+          </section>
+          <section className='col-12 col-md-5 col-lg-4 p-3'>
+            <h3>Trailer</h3>
+            <iframe width="100%" height="auto" src={movie.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          </section>
+          <section className=''>
+            <CardRelationMovie></CardRelationMovie>
           </section>
         </section>
       </Tab>
